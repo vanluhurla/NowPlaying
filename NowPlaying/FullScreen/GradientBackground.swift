6e2aea6e0cfc5de4b006b20ec9c724a2b075f85a
Vanessa Lucena Hurla <vanlucenasilva@gmail.com>
2025-08-05 17:02:52 +0100

    Create screen play view with swipe-down to collapse animation.

---
+++ NowPlaying/FullScreen/GradientBackground.swift
@@ -0,0 +1,41 @@
+//
+//  GradientBackground.swift
+//  NowPlaying
+//
+//  Created by Vanessa Lucena on 05/08/2025.
+//
+
+import SwiftUI
+
+struct GradientBackground: View {
+    @State private var animate = false
+    
+    var body: some View {
+        ZStack {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                Circle()
+                    .fill(.background.opacity(0.9))
+                    .frame(width: 700, height: 900)
+                    .offset(x: 0, y: -100)
+                    .blur(radius: 50)
+                
+                Circle()
+                    .fill(.background.opacity(0.2))
+                    .frame(width: 500, height: 400)
+                    .offset(x: -300, y: -80)
+                    .blur(radius: 40)
+                
+                Circle()
+                    .fill(.background.opacity(0.15))
+                    .frame(width: 100, height: 800)
+                    .offset(x: 0, y: -300)
+                    .blur(radius: 45)
+            }
+        }
+    }
+}
+
+#Preview {
+    GradientBackground()
+}
