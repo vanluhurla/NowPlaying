0e7a42bb758dffe4e72e2b7fcf5fb2e90386d4cf
Vanessa Lucena Hurla <vanlucenasilva@gmail.com>
2025-08-05 10:52:48 +0100

    Initial commit

---
+++ NowPlaying/NowPlayingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingApp.swift
+//  NowPlaying
+//
+//  Created by Vanessa Lucena on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
